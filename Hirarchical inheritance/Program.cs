f40c6719809dd1e41d45dac176022dadb69e592b
nomvula <nzshabangu95@gmail.com>
2023-09-15 05:07:21 +0200

    Add project files.

---
+++ Hirarchical inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hirarchical_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B obj = new B();
+            Console.WriteLine(obj.info());
+            Console.ReadLine();
+        }
+    }
+
+    class A  //base class
+    {
+        public string msg()
+        {
+            return "this is A class Method";
+        }
+    }
+    class B : A
+    {
+        public string info()
+        {
+            msg();
+            return "this is B class Method";
+        }
+        class C : A
+        {
+            public string getinfo()
+            {
+                msg();
+                return "this is B class Method";
+            }
+        }
+    }
+}
